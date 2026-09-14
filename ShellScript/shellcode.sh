c995ef2682010a56a96bf1b200c0f291ec79619b
rohintuni88 <rohintuni88@gmail.com>
2026-09-14 20:32:56 +0800

    code

--- ShellScript/shellcode.sh
+++ ShellScript/shellcode.sh
@@ -12,3 +12,17 @@ echo "Script name: $0"
 echo "Current user: $USER"
 echo "Home directory: $HOME"
 echo "Current working directory: $PWD"
+
+NUM1=20
+NUM2=40
+SUM=$((NUM1 + NUM2))
+echo "Sum of $NUM1 and $NUM2 is: $SUM"
+
+ MOVIES=("RRR", "DDD", "KGF", "PUSHPA")
+ echo "display ${MOVIES[@]}"
+ echo "display ${MOVIES[0]}"
+ echo "display ${MOVIES[1]}"
+ echo "display ${MOVIES[2]}"
+ echo "display ${MOVIES[3]}"
+ echo $?
+ echo $$
