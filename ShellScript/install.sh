fa14d1f6fbaf67b99ac950ba62ed0add64504b36
rohintuni88 <rohintuni88@gmail.com>
2026-09-15 08:37:03 +0800

    code

---
+++ ShellScript/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+USERID=$(uid -u)
+if [ $USERID -ne 0 ]; then
+{
+ echo "inside if condition $USERID"
+ exit 1
+} else {
+    dnf install mysql -y
+}
+fi
+
+if [ $? -ne 0]; then
+{
+    echo "Fail"
+} else {
+    echo "Sucess"
+}
+
