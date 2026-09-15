ae93824f23073bb3766fa298e872bc74dd835814
rohintuni88 <rohintuni88@gmail.com>
2026-09-15 09:37:10 +0800

    code

--- ShellScript/install.sh
+++ ShellScript/install.sh
@@ -6,6 +6,8 @@ if [ $USERID -ne 0 ]; then
      echo "Please use Sudo user"
     exit 1
 fi
+
+#  Without Finctions, to install mysql
  dnf install mysql -y
 if [ $? -eq 0 ]; then 
     echo "Already Installed"
@@ -14,9 +16,22 @@ if [ $? -eq 0 ]; then
 else
     echo "Installing now $?"
     dnf install mysql -y
-     if [ $? -ne 0 ]; then
-        echo " Installing fail $?"
-    else 
-        echo "Installing Sucess $?"
-    fi
+    VALIDATE MySql $?
+ #    if [ $? -ne 0 ]; then
+ #        echo " Installing fail $?"
+ #     else 
+ #        echo "Installing Sucess $?"
+ #    fi
 fi
+
+ 
+
+# with Funtions
+
+  VALIDATE(){
+     if [ $2 -ne 0 ]; then
+        echo " Installing fail $1"
+    else 
+        echo "Installing Sucess $1"
+    fi
+  }
