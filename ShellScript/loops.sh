2e3cd31ec15d015091aa99fe6b596e3b9d9d748c
rohintuni88 <rohintuni88@gmail.com>
2026-09-17 19:50:15 +0800

    Loops

--- ShellScript/loops.sh
+++ ShellScript/loops.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 LOG_FOLDER=/var/log/shell
 LOG_FILES="$LOG_FOLDER/$0.log"
 TIME_STAMP=$(date "+%Y-%m-%d")
+R="\e[31m"
 
 if [ $USERID -ne 0 ]; then
   echo "USE Sudo user"
@@ -12,15 +13,15 @@ fi
 
 for pack in $@
 do
-  echo $TIME_STAMP "install..............................................HELLOOOOOOOOOOOOOOOOOOOOOO" $pack  &>> $LOG_FILES | tee -a $LOG_FILES
-  dnf install $pack -y  &>> $LOG_FILES | tee -a $LOG_FILES
+  echo $TIME_STAMP "$R install..............................................HELLOOOOOOOOOOOOOOOOOOOOOO" $pack  &>> $LOG_FILES | tee -a $LOG_FILES
+  dnf install $pack -y  &>> $R $LOG_FILES | tee -a $LOG_FILES
   dnf list installed $pack &>> $LOG_FILES | tee -a $LOG_FILES
   
   if [ $? -ne 0 ]; then
    dnf install $pack -y &>> $LOG_FILES
    echo " $TIME_STAMP Installed ................ $pack" | tee -a $LOG_FILES
    else
-   echo "$TIME_STAMP Not Installed ..............$pack" | tee -a $LOG_FILES
+   echo "$R $TIME_STAMP Not Installed ..............$pack" | tee -a $LOG_FILES
    fi
 done
   if [ $? -ne 0 ]; then
