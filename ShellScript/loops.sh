988d20f5d39d04ebbc5937106809ccec8e0311a9
rohintuni88 <rohintuni88@gmail.com>
2026-09-17 19:34:35 +0800

    Loops

--- ShellScript/loops.sh
+++ ShellScript/loops.sh
@@ -13,7 +13,7 @@ fi
 for pack in $@
 do
   echo $TIME_STAMP "install" $pack  &>> $LOG_FILES | tee -a $LOG_FILES
-  dnf install $pack -y  &>> $LOG_FILES | tee -a $LOG_FILES
+  dnf remove $pack -y  &>> $LOG_FILES | tee -a $LOG_FILES
   dnf list installed $pack &>> $LOG_FILES | tee -a $LOG_FILES
   
   if [ $? -ne 0 ]; then
