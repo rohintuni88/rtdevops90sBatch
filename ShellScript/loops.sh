b80b9778d695dd5d3bf7061cce06629029258eac
rohintuni88 <rohintuni88@gmail.com>
2026-09-17 19:24:49 +0800

    Loops

--- ShellScript/loops.sh
+++ ShellScript/loops.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 LOG_FOLDER=/var/log/shell
 LOG_FILES="$LOG_FOLDER/$0.log"
+TIME_STAMP=$(date "+%Y-%m-%d")
 
 if [ $USERID -ne 0 ]; then
   echo "USE Sudo user"
@@ -11,13 +12,13 @@ fi
 
 for pack in $@
 do
-  echo insall $pack
+  echo "insall $pack  &>> $LOG_FILES" | tee -a $LOG_FILES
   dnf install $pack -y  &>> $LOG_FILES
   dnf list installed $pack &>> $LOG_FILES
   
   if [ $? -ne 0 ]; then
    dnf install $pack -y &>> $LOG_FILES
-   echo "Installed ................ $pack"
+   echo " $TIME_STAMP Installed ................ $pack"
    else
    echo "Not Installed ..............$pack"
    fi
