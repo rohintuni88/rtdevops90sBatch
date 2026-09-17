2e3636cd55fee0162b11166cc59b6f2f099ee877
rohintuni88 <rohintuni88@gmail.com>
2026-09-17 19:30:32 +0800

    Loops

--- ShellScript/loops.sh
+++ ShellScript/loops.sh
@@ -12,15 +12,15 @@ fi
 
 for pack in $@
 do
-  echo "$TIME_STAMP insall" $pack  &>> $LOG_FILES | tee -a $LOG_FILES
-  dnf install $pack -y  &>> $LOG_FILES
-  dnf list installed $pack &>> $LOG_FILES
+  echo $TIME_STAMP "install" $pack  &>> $LOG_FILES | tee -a $LOG_FILES
+  dnf install $pack -y  &>> $LOG_FILES | tee -a $LOG_FILES
+  dnf list installed $pack &>> $LOG_FILES | tee -a $LOG_FILES
   
   if [ $? -ne 0 ]; then
    dnf install $pack -y &>> $LOG_FILES
-   echo " $TIME_STAMP Installed ................ $pack"
+   echo " $TIME_STAMP Installed ................ $pack" | tee -a $LOG_FILES
    else
-   echo "$TIME_STAMP Not Installed ..............$pack"
+   echo "$TIME_STAMP Not Installed ..............$pack" | tee -a $LOG_FILES
    fi
 done
   if [ $? -ne 0 ]; then
